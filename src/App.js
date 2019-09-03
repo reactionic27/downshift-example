a41efb752d202558eceb7145a753d128bd171acb
reactionic127 <richard.gill127@gmail.com>
2019-09-03 04:00:40 +0100

    added downshift module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import Downshift from 'downshift';
 import './App.css';
+
+const items = [
+  {value: 'apple'},
+  {value: 'pear'},
+  {value: 'orange'},
+  {value: 'grape'},
+  {value: 'banana'},
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Downshift
+          onChange={selection => alert(
+            selection ? `You selected ${selection.value}` : 'Selection Cleared'
+          )}
+          itemToString={item => (item ? item.value : '')}
         >
-          Learn React
-        </a>
+          {({
+            getInputProps,
+            getItemProps,
+            getLabelProps,
+            getMenuProps,
+            isOpen,
+            inputValue,
+            highlightedIndex,
+            selectedItem,
+          }) => (
+            <div>
+              <label {...getLabelProps()}>Enter a fruit</label>
+              <input {...getInputProps()} />
+              <ul {...getMenuProps()}>
+                {isOpen
+                  ? items
+                      .filter(item => !inputValue || item.value.includes(inputValue))
+                      .map((item, index) => (
+                        <li
+                          {...getItemProps({
+                            key: item.value,
+                            index,
+                            item,
+                            style: {
+                              backgroundColor:
+                                highlightedIndex === index ? 'lightgray' : 'white',
+                              fontWeight: selectedItem === item ? 'bold' : 'normal',
+                            },
+                          })}
+                        >
+                          {item.value}
+                        </li>
+                      ))
+                  : null}
+              </ul>
+            </div>
+          )}
+        </Downshift>
       </header>
     </div>
   );
